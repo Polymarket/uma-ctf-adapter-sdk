00afb6f95b14c351088ed6c30073df18fc82d238
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2022-10-27 16:44:07 -0400

    fix: add contract addresses

--- src/networks.ts
+++ src/networks.ts
@@ -1,9 +1,9 @@
 export const getAdapterAddress = (chainID: number): string => {
     switch (chainID) {
         case 137:
-            return ""; // TODO
+            return "0x6A9D222616C90FcA5754cd1333cFD9b7fb6a4F74";
         case 80001:
-            return ""; // TODO
+            return "0x6A9D222616C90FcA5754cd1333cFD9b7fb6a4F74";
         default:
             throw new Error(`Unsupported chainID: ${chainID}`);
     }
