77bd6ab40614bf78223679659e30ec6c09a0ff38
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2023-06-01 13:53:08 -0400

    chore: validate chainID

--- src/client/base.ts
+++ src/client/base.ts
@@ -10,8 +10,19 @@ export class BaseAdapterClient {
     contract: Contract;
 
     constructor(signer: JsonRpcSigner | Wallet, chainID: ChainID, abi: ContractInterface, contractAddress: string) {
+        BaseAdapterClient._validateChainID(chainID);
         this.chainID = chainID;
         this.signer = signer;
         this.contract = new Contract(contractAddress, abi, signer)
+    }
+
+    public getAddress(): string {
+        return this.contract.address;
+    }
+
+    private static _validateChainID(chainID: number): void {
+        if(!(chainID == ChainID.MUMBAI || chainID == ChainID.POLYGON)) {
+            throw new Error(`Invalid chainID ${chainID}`);
+        }
     }
 }
